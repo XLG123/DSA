97daec352bfcc0dcb622a71c2e7b45d8669d32ed
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-08-20 17:38:05 -0400

    number 91 in progress

--- arrays.js
+++ arrays.js
@@ -1,1 +1,27 @@
-// This file will be used to practice array questions
+// This file will be used to practice array questions/**
+
+
+// 91
+/*
+ * @param {string} s
+ * @return {number}
+*/
+var numDecodings = function(s) {
+    let keys = {};
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < 26; ++i) {
+        keys[i+1] = letters[i].toUpperCase(); 
+    }
+
+    if (s[0] === "0") {
+        return 0;
+    } else if (s.includes("0") && parseInt(s[s.indexOf("0")-1]) > 2) {
+        return 0;
+    } else {
+        let i = 0;
+        let increment = 0;
+        let count = 0;
+        // while ()
+    }
+
+};
