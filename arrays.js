a76b7b068f8e4b92c751d495e28614f5bb80217d
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-08-24 11:02:37 -0400

    updates

--- arrays.js
+++ arrays.js
@@ -7,21 +7,21 @@
  * @return {number}
 */
 var numDecodings = function(s) {
-    let keys = {};
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    for (let i = 0; i < 26; ++i) {
-        keys[i+1] = letters[i].toUpperCase(); 
-    }
+    // let keys = {};
+    // const letters = "abcdefghijklmnopqrstuvwxyz";
+    // for (let i = 0; i < 26; ++i) {
+    //     keys[i+1] = letters[i].toUpperCase(); 
+    // }
 
-    if (s[0] === "0") {
-        return 0;
-    } else if (s.includes("0") && parseInt(s[s.indexOf("0")-1]) > 2) {
-        return 0;
-    } else {
-        let i = 0;
-        let increment = 0;
-        let count = 0;
+    // if (s[0] === "0") {
+    //     return 0;
+    // } else if (s.includes("0") && parseInt(s[s.indexOf("0")-1]) > 2) {
+    //     return 0;
+    // } else {
+    //     let i = 0;
+    //     let increment = 0;
+    //     let count = 0;
 
-    }
-    // console.log()
+    // }
+    // // console.log()
 };
