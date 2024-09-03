d72072db08d9b5ea09cbb2bdbcf880c2983e0020
XLG123 <98980493+XLG123@users.noreply.github.com>
2024-09-03 13:13:59 -0400

    dsa

--- arrays.js
+++ arrays.js
@@ -1,27 +1,45 @@
 // This file will be used to practice array questions/**
+const p = console.log.bind(console);
+p(1);
 
 
 // 91
-/*
- * @param {string} s
+// ...
+
+
+// 1051 Height Checker
+// Example 1:
+
+// Input: heights = [1,1,4,2,1,3]
+// Output: 3
+// Explanation: 
+// heights:  [1,1,4,2,1,3]
+// expected: [1,1,1,2,3,4]
+// Indices 2, 4, and 5 do not match.
+// Example 2:
+
+// Input: heights = [5,1,2,3,4]
+// Output: 5
+// Explanation:
+// heights:  [5,1,2,3,4]
+// expected: [1,2,3,4,5]
+// All indices do not match.
+// Example 3:
+
+// Input: heights = [1,2,3,4,5]
+// Output: 0
+// Explanation:
+// heights:  [1,2,3,4,5]
+// expected: [1,2,3,4,5]
+// All indices match.
+/**
+ * @param {number[]} heights
  * @return {number}
-*/
-var numDecodings = function(s) {
-    // let keys = {};
-    // const letters = "abcdefghijklmnopqrstuvwxyz";
-    // for (let i = 0; i < 26; ++i) {
-    //     keys[i+1] = letters[i].toUpperCase(); 
-    // }
-
-    // if (s[0] === "0") {
-    //     return 0;
-    // } else if (s.includes("0") && parseInt(s[s.indexOf("0")-1]) > 2) {
-    //     return 0;
-    // } else {
-    //     let i = 0;
-    //     let increment = 0;
-    //     let count = 0;
-
-    // }
-    // // console.log()
+ */
+var heightChecker = function(heights) {
+    
 };
+
+p(heightChecker([1, 1, 4, 2, 1, 3]));
+p(heightChecker([5, 1, 2, 3, 4]));
+p(heightChecker([1, 2, 3, 4, 5]));
